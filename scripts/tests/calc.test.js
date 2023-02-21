4ac7c64403219873eba3c25378492fa4e9dd9e2a
Murilo Lima <msl@cesar.school>
2023-02-21 21:05:18 +0000

    Test with mock

--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -1,3 +1,7 @@
+/**
+ * @jest-environment jsdom
+ */
+
 const { TestScheduler } = require('jest');
 const addition = require('../calc');
 
@@ -6,6 +10,9 @@ describe('Calculator', () => {
     describe('Addition function', () => {
         test('should return 42 for 10 + 22', () => {
             expect(addition(20, 22)).toBe(42);
+        });
+        test('should return 73 for 42 + 31', () => {
+            expect(addition(42, 31)).toBe(73);
         })
     });
     describe('Suctraction function', () => {
