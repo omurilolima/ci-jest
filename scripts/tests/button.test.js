2af95e5faee1bd98fe7242dc49b2fe6dd02573b7
Murilo Lima <msl@cesar.school>
2023-02-22 15:04:17 +0000

    Testing the DOM

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -2,15 +2,25 @@
  * @jest-environment jsdom
  */
 
+// BEFORE TESTING, install Jest with this command: npm install --save-dev jest@26.6.3
+
 const buttonClick = require('../button');
 
 beforeEach(() => {
-    document.body.innerHTML = "<p id='par'></p>";
+    // read the entire index.html for mocking
+    let fs = require('fs'); // File system handling module built into Node that allos us to open, read and write files.
+    let fileContents = fs.readFileSync('index.html', 'utf-8'); // read the contents of the HTML file.
+    document.open();
+    document.write(fileContents);
+    document.close();
 });
 
 describe('DOM tests', () => {
     test('expects p content to change', () => {
         buttonClick();
         expect(document.getElementById('par').innerHTML).toEqual('You Clicked');
-    })
+    });
+    test('h1 should exist', () => {
+        expect(document.getElementsByTagName('h1').length).toBe(1);
+    });
 });
