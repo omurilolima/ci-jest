db07b5cb00d2420dbc71297edb594957bce74bcb
Murilo Lima <msl@cesar.school>
2023-02-22 16:22:18 +0000

    Add game object tests

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -1,3 +1,28 @@
 /**
  * @jest-environment jsdom
  */
+
+const { game } = require('../game');
+
+beforeAll(() => {
+    let fs = require('fs');
+    let fileContents = fs.readFileSync('index.html', 'utf-8');
+    document.open();
+    document.write(fileContents);
+    document.close();
+})
+
+describe('game object contains correct keys', () => {
+    test('score keys exists', () => {
+        expect('score' in game).toBe(true);
+    });
+    test('currentGame keys exists', () => {
+        expect('currentGame' in game).toBe(true);
+    });
+    test('playerMoves keys exists', () => {
+        expect('playerMoves' in game).toBe(true);
+    });
+    test('choices keys exists', () => {
+        expect('choices' in game).toBe(true);
+    });
+});
