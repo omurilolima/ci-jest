18f27e52f6d9c7ad7ef3bd3833915798bc6ec83d
Murilo Lima <msl@cesar.school>
2023-02-22 20:25:13 +0000

    Add eventlistener for the circles

--- scripts/game.js
+++ scripts/game.js
@@ -2,13 +2,25 @@ let game = {
     currentGame: [],
     playerMoves: [],
     score: 0,
-    choices: ["button1", "button2", "button3", "button4"]
+    turnNumber: 0,
+    choices: ["button1", "button2", "button3", "button4"],
 };
 
 function newGame() {
+    game.score = 0;
     game.currentGame = [];
     game.playerMoves = [];
-    game.score = 0;
+    for (let circle of document.getElementsByClassName('circle')) {
+        if (circle.getAttribute('data-listener') !== 'true') {
+            circle.addEventListener('click', (e) => {
+                let move = e.target.getAttribute('id');
+                lightsOn(move);
+                game.playerMoves.push(move);
+                playerTurn();
+            });
+            circle.setAttribute('data-listener', 'true');
+        }
+    };
     showScore();
     addTurn();
 }
@@ -16,7 +28,18 @@ function newGame() {
 function addTurn() {
     game.playerMoves = [];
     game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
-    // showTurns();
+    showTurns();
+}
+
+function showTurns() {
+    game.turnNumber = 0;
+    let turns = setInterval(function () {
+        lightsOn(game.currentGame[game.turnNumber]);
+        game.turnNumber++;
+        if (game.turnNumber >= game.currentGame.length) {
+            clearInterval(turns);
+        }
+    }, 800);
 }
 
 function lightsOn(circ) {
@@ -30,4 +53,4 @@ function showScore() {
     document.getElementById("score").innerText = game.score;
 }
 
-module.exports = { game, newGame, showScore, addTurn, lightsOn };
+module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns };
